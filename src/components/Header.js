adaa9989df75ad23de3d9fc6a855ce1f8cc79cbf
Rolake Olonisakin <rollyloni@gmail.com>
2022-04-11 22:27:03 -0700

    add all jsx of header component ans style with scss

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import "./Header.scss";
+import brnflixLogo from "./assets/logo/BrainFlix-logo.svg";
+import searchIcon from "./assets/Icons/search.svg";
+import avatar from "./assets/Images/Mohan-muruge.jpg";
+import uploadIcon from "./assets/Icons/upload.svg";
+
+function Header() {
+  return (
+    <header className="header-section">
+      <img
+        className="header-section__logo"
+        src={brnflixLogo}
+        alt="logo for brainflix"
+      ></img>
+      <section className="header-section__div">
+        <img
+          className="header-section__search"
+          src={searchIcon}
+          alt="search icon"
+        ></img>
+        <input type="text" placeholder="Search"></input>
+        <img
+          className="header-section__avatar"
+          src={avatar}
+          alt="avatar icon"
+        ></img>
+      </section>
+      <img
+        className="header-section__button--logo"
+        src={uploadIcon}
+        alt="upload icon"
+      ></img>
+      <section className="header-section__button"></section>
+      <button>UPLOAD</button>
+    </header>
+  );
+}
+
+export default Header;
