9f2295111ef97c25cdfcb5d3af9007af4541d98d
Rolake Olonisakin <rollyloni@gmail.com>
2022-04-24 16:15:31 -0700

    final commit

--- src/Pages/VideoUploadPage.js
+++ src/Pages/VideoUploadPage.js
@@ -5,38 +5,42 @@ import uploadIcon from "../components/assets/Icons/publish.svg";
 
 const VideoUploadPage = () => {
   return (
-    <div>
-      <form>
-        <h1 className="video__heading">Upload Video</h1>
-        <p className="video__title">VIDEO THUMBNAIL</p>
-        <img
-          src={thumbnailImg}
-          alt="Video preview"
-          className="video__img"
-        ></img>
-        <p className="video__title">TITLE YOUR VIDEO</p>
-        <input
-          placeholder="Add a title to your video"
-          className="video__input"
-        />
-        <p className="video__title">ADD A VIDEO DESCRIPTION</p>
-        <textarea
-          placeholder="Add a description to your video"
-          className="video__textarea"
-        />
-        <div className="video__button--div">
-          <button>
-            <img
-              className="header-section__button--logo"
-              src={uploadIcon}
-              alt="upload icon"
-            ></img>
-            PUBLISH
-          </button>
-          <p className="cancel__button">CANCEL</p>
+    <form>
+      <h1 className="video__heading">Upload Video</h1>
+      <div className="video__div">
+        <div>
+          <p className="video__title">VIDEO THUMBNAIL</p>
+          <img
+            src={thumbnailImg}
+            alt="Video preview"
+            className="video__img"
+          ></img>
         </div>
-      </form>
-    </div>
+        <div className="test">
+          <span className="video__title">TITLE YOUR VIDEO</span>
+          <input
+            placeholder="Add a title to your video"
+            className="video__input"
+          />
+          <span className="video__title">ADD A VIDEO DESCRIPTION</span>
+          <textarea
+            placeholder="Add a description to your video"
+            className="video__textarea"
+          />
+        </div>
+      </div>
+      <div className="video__button--div">
+        <button className="publish__button">
+          <img
+            className="header-section__button--logo"
+            src={uploadIcon}
+            alt="upload icon"
+          ></img>
+          PUBLISH
+        </button>
+        <p className="cancel__button">CANCEL</p>
+      </div>
+    </form>
   );
 };
 
